900adfd77c479b1f35355ef0b01c8a4a0d62cefe
Alexander520Ga <83433911+Alexander520Ga@users.noreply.github.com>
2022-01-10 12:32:23 -0600

    v1 CalendarApp

---
+++ public/precache-manifest.961342caa97589a9b99dd4d01d7e779f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb867ada1f2f2e56e338b96ae558b56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "399a786cb4cb18a1d2dd",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "13f3ff0a4c57a2196506",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "399a786cb4cb18a1d2dd",
+    "url": "/static/js/2.00608677.chunk.js"
+  },
+  {
+    "revision": "5d8c8be002cd0ed21fa2e0cc704f2235",
+    "url": "/static/js/2.00608677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f3ff0a4c57a2196506",
+    "url": "/static/js/main.deb0909f.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
